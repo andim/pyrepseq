a8387541676bbcb32dce4d30859d0ed68d042c68
Andreas Mayer <andisspam@gmail.com>
2021-04-08 11:32:27 -0400

    started pyrepseq

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    
+ver_file = os.path.join('pyrepseq', 'version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+opts = dict(name=NAME,
+            maintainer=MAINTAINER,
+            maintainer_email=MAINTAINER_EMAIL,
+            description=DESCRIPTION,
+            long_description=LONG_DESCRIPTION,
+            url=URL,
+            download_url=DOWNLOAD_URL,
+            license=LICENSE,
+            classifiers=CLASSIFIERS,
+            author=AUTHOR,
+            author_email=AUTHOR_EMAIL,
+            platforms=PLATFORMS,
+            version=VERSION,
+            packages=PACKAGES,
+            package_data=PACKAGE_DATA,
+            install_requires=REQUIRES)
+
+
+if __name__ == '__main__':
+    setup(**opts)
