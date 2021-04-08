1e11eb8f56d3f55fbcfaadfce465dbcc6a842011
Andreas Mayer <andisspam@gmail.com>
2021-04-08 18:35:48 -0400

    find neighbor pairs added

--- pyrepseq/neighbors.py
+++ pyrepseq/neighbors.py
@@ -1,7 +1,7 @@
 from .main import aminoacids
 
 def levenshtein_neighbors(x, alphabet=aminoacids):
-    """Iterator over Levenshtein neighbors of a sequence x"""
+    """Iterator over Levenshtein neighbors of a string x"""
     for i in range(len(x)):
         # deletion
         yield x[:i]+x[i+1:]
@@ -18,10 +18,23 @@ def levenshtein_neighbors(x, alphabet=aminoacids):
         yield x+aa
 
 def hamming_neighbors(x, alphabet=aminoacids):
-    """Iterator over Hamming neighbors of a sequence x"""
+    """Iterator over Hamming neighbors of a string x"""
     for i in range(len(x)):
         for aa in alphabet:
             if aa == x[i]:
                 continue
             yield x[:i]+aa+x[i+1:]
  
+def find_neighbor_pairs(seqs, neighborhood=hamming_neighbors):
+    """Find neighboring sequences in a list.
+
+    neighborhood: callable returning an iterable of neighbors
+    """
+    reference = set(seqs)
+    pairs = []
+    for x in set(seqs):
+        for y in neighborhood(x):
+            if y in reference:
+                pairs.append((x, y))
+        reference.remove(x)
+    return pairs
