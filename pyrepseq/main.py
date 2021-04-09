294dd3a16e72a789fd75b8533752cbb12ce1fdee
Andreas Mayer <andisspam@gmail.com>
2021-04-08 23:00:52 -0400

    simplified coincidence probability formula

--- pyrepseq/main.py
+++ pyrepseq/main.py
@@ -6,7 +6,10 @@ aminoacids = 'ACDEFGHIKLMNPQRSTVWY'
 def coincidence_probability(array):
     """
     Calculates probability that two distinct elements of a list are the same.
+
+    Note: this is also known as the Simpson or Hunter-Gaston index
     """
     array = np.asarray(array)
     _, counts = np.unique(array, return_counts=True)
-    return 2*np.sum(scipy.special.binom(counts[counts>1], 2))/(array.shape[0]*(array.shape[0]-1))
+    # 2*(n choose 2) = n * (n-1)
+    return np.sum(counts*(counts-1))/(array.shape[0]*(array.shape[0]-1))
