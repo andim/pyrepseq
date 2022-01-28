05742698a153a35dcace97b790a75e7c20436219
Andreas Mayer <andisspam@gmail.com>
2022-01-28 18:26:05 +0100

    updated cdr3 validation code

--- pyrepseq/io.py
+++ pyrepseq/io.py
@@ -2,9 +2,12 @@ from .main import isvalidaa
 
 def isvalidcdr3(string):
     """
-    returns True if string is a valid CDR3 alpha sequence
+    returns True if string is a valid CDR3 sequence
 
-    Checks whether first aa is cysteine (C), and last aa is phenylalanine (F) or tryptophan (W).
+    Checks the following:
+        - first amino acid is a cysteine (C)
+        - last amino acid is either phenylalanine (F) or tryptophan (W)
+        - each amino acid is part of the standard amino acid alphabet
     See http://www.imgt.org/IMGTScientificChart/Numbering/IMGTIGVLsuperfamily.html
     """
     return isvalidaa(string) and (string[0] == 'C') and (string[-1] in ['F', 'W'])
