3e0c65e0777471c103664549fa5f10c7a0dc6ac2
Andreas Mayer <andisspam@gmail.com>
2022-06-23 10:20:42 +0100

    expanded cdr3 conserved residue definition (see heather nar 2022)

--- pyrepseq/io.py
+++ pyrepseq/io.py
@@ -6,8 +6,11 @@ def isvalidcdr3(string):
 
     Checks the following:
         - first amino acid is a cysteine (C)
-        - last amino acid is either phenylalanine (F) or tryptophan (W)
+        - last amino acid is either phenylalanine (F), tryptophan (W), or cysteine (C)
         - each amino acid is part of the standard amino acid alphabet
     See http://www.imgt.org/IMGTScientificChart/Numbering/IMGTIGVLsuperfamily.html
+    and also https://doi.org/10.1093/nar/gkac190
     """
-    return isvalidaa(string) and (string[0] == 'C') and (string[-1] in ['F', 'W'])
+    return (isvalidaa(string)
+            and (string[0] == 'C')
+            and (string[-1] in ['F', 'W', 'C'])
