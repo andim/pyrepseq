12a077f8d102f16e1a6c6adcc6e27ab714da6823
Andreas Mayer <andisspam@gmail.com>
2022-08-11 18:06:27 +0100

    multimerge fixes

--- pyrepseq/main.py
+++ pyrepseq/main.py
@@ -10,7 +10,7 @@ def isvalidaa(string):
     "returns true if string is composed only of characters from the standard amino acid alphabet"
     return all(c in _aminoacids_set for c in string)
 
-def multimerge(dfs, on, suffixes=None):
+def multimerge(dfs, on, suffixes=None, **kwargs):
     """Merge multiple dataframes on a common column.
 
     Provides support for custom suffixes.
@@ -18,11 +18,17 @@ def multimerge(dfs, on, suffixes=None):
     Parameters
     ----------
     on: 'index' or column name
+    suffixes: [list-like | None]
+        list of suffixes to append to the data
+    **kwargs:  keyword arguments passed along to `pd.merge`
 
     Returns
     -------
     merged dataframe
     """
+
+    merge_kwargs = dict(how='outer')
+    merge_kwargs.update(kwargs)
     if suffixes:
         dfs_new = []
         for df, suffix in zip(dfs, suffixes):
@@ -31,6 +37,12 @@ def multimerge(dfs, on, suffixes=None):
             dfs_new.append(df.add_suffix('_'+suffix))
         return reduce(lambda left, right: pd.merge(left, right,
                                                    right_index=True, left_index=True,
-                                                   how='outer'),
+                                                   **merge_kwargs),
                       dfs_new)
-    return reduce(lambda left, right: pd.merge(left, right, on), dfs)
+    if on == 'index':
+        return reduce(lambda left, right: pd.merge(left, right,
+                                                   right_index=True, left_index=True,
+                                                   **merge_kwargs),
+                      dfs)
+    return reduce(lambda left, right: pd.merge(left, right, on,
+                                               **merge_kwargs), dfs)
