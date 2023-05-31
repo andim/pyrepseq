685e87c071c3067ac6ec1496afca003d83f36867
Andreas Mayer <andisspam@gmail.com>
2023-05-31 19:50:05 +0100

    pep8 compliance, more comments

--- pyrepseq/tcr_info.py
+++ pyrepseq/tcr_info.py
@@ -6,35 +6,34 @@ import numpy as np
 import pandas as pd
 
 
-#Compute entropy of a feature from coincidence probability
-def renyi2_entropy(df, features, by = [], log_fun = np.log2):
+def renyi2_entropy(df, features, by=None, base=2.0):
+    """Compute Renyi-Simpson entropies.
+    """
 
-    "Estimator for Renyi entropy of order 2"
-    
-    #Check if multiple features are given or a single feature
+    if base is not None and base <= 0:
+        raise ValueError("`base` must be a positive number or `None`.")
+
+    # Check if multiple features are given or a single feature
     if type(features) == str:
-        
         features = [features]
     
-    #Check for single term groupby
-    if len(by) == 1:
-        
+    # Check for single term groupby
+    if by is not None and len(by) == 1:
         by = by[0]
     
-    #Check to see if conditioning is required
     if not by:
-        
-        entropy = -log_fun(pc_joint(df, features))
-        
+        entropy = -np.log(pc_joint(df, features))
     else:
-        
-        entropy = -log_fun(pc_conditional(df, by, features))
-        
-    
+        entropy = -np.log(pc_conditional(df, by, features))
+
+    if base is not None:
+        entropy /= np.log(base) 
     return entropy
 
 
-def feature_relevance_dict(df_spc, df_back, features, by = [], spc_group_column=None, *args, **kwargs):
+def feature_relevance_dict(df_spc, df_back, features, by=[], spc_group_column=None, *args, **kwargs):
+    """ Calculate a dictionary of feature relevance scores.
+    """
     
     if not spc_group_column:
         by_spc = by
@@ -43,19 +42,14 @@ def feature_relevance_dict(df_spc, df_back, features, by = [], spc_group_column=
         by_spc = by + [spc_group_column]
         
     relevance_dict = {}
-    #Iterate through the features
     for i, feature in enumerate(features):
-        
-        #Compute lone relevance
+        # Compute relevance
         relevance_dict[feature] = (renyi2_entropy(df_back, feature, by) - renyi2_entropy(df_spc, feature, by_spc))
-        
-           
-        ##Compute conditional and joint relevance
         for feature_2 in np.delete(features,i):
+            # Compute conditional relevance
             relevance_dict[feature+"|"+feature_2] =  (renyi2_entropy(df_back, feature, by + [feature_2])
                                                         -  renyi2_entropy(df_spc, feature, by_spc + [feature_2]))
-      
-      
+            # Compute joint relevance
             relevance_dict[feature+"+"+feature_2] =  (renyi2_entropy(df_back, [feature, feature_2], by)
                                                         -  renyi2_entropy(df_spc, [feature, feature_2], by_spc))
     
