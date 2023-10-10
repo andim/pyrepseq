7fe5813232bcf05308fe1fe1a9a2f8bca9566a9d
heartnetkung <heartnetkung@gmail.com>
2023-10-10 18:14:04 +0100

    remove return order from the specification

--- tests/test_nearest_neighbor.py
+++ tests/test_nearest_neighbor.py
@@ -1,11 +1,11 @@
 import pytest
-from pyrepseq.nearest_neighbor import hash_based, kdtree
+from pyrepseq.nearest_neighbor import hash_based, kdtree, symspell
 from itertools import product
 from Levenshtein import distance
 import numpy as np
 import pandas as pd
 
-ALGORITHMS = [kdtree, hash_based]
+ALGORITHMS = [kdtree, hash_based, symspell]
 fallback_cache = None
 
 
@@ -24,18 +24,24 @@ def fallback(seqs, max_edits=1):
     return ans
 
 
+def set_equal(list_a, list_b):
+    # normal set equallity is slow in python, so we implement one
+    set_a, set_b = set(list_a), set(list_b)
+    return len(set_a) == len(set_b) == len(set_a & set_b)
+
+
 @pytest.mark.parametrize("algorithm", ALGORITHMS)
 def test_basic(algorithm):
     test_input = ['CAAA', 'CDDD', 'CADA', 'CAAK']
     test_output = [(0, 2, 1), (0, 3, 1), (2, 0, 1), (3, 0, 1)]
-    assert algorithm(test_input, max_edits=1) == test_output
+    assert set_equal(algorithm(test_input, max_edits=1), test_output)
 
     fallback_version = fallback(test_input)
-    assert fallback_version == test_output
+    assert set_equal(fallback_version, test_output)
 
     test_output = [(0, 2, 1), (2, 0, 1), (3, 0, 1)]
-    assert algorithm(test_input, max_edits=1,
-                     max_returns=1) == test_output
+    assert set_equal(algorithm(test_input, max_edits=1,
+                               max_returns=1), test_output)
 
 
 @pytest.mark.parametrize("algorithm", ALGORITHMS)
@@ -43,51 +49,51 @@ def test_duplicate(algorithm):
     test_input = ['CAAA', 'CDDD', 'CADA', 'CAAA']
     test_output = [(0, 3, 0), (0, 2, 1), (2, 0, 1),
                    (2, 3, 1), (3, 0, 0), (3, 2, 1)]
-    assert algorithm(test_input, max_edits=1) == test_output
+    assert set_equal(algorithm(test_input, max_edits=1), test_output)
 
     fallback_version = fallback(test_input)
-    assert sorted(fallback_version) == sorted(test_output)
+    assert set_equal(fallback_version, test_output)
 
-    test_output = [(0, 3, 0), (2, 0, 1), (3, 0, 0)]
-    assert algorithm(test_input, max_edits=1, max_returns=1) == test_output
+    output = set(algorithm(test_input, max_edits=1, max_returns=1))
+    assert len(output) == 3 == len(set(test_output) & output)
 
 
 @pytest.mark.parametrize("algorithm", ALGORITHMS)
 def test_compatibility(algorithm):
     test_input = np.array(['CAAA', 'CDDD', 'CADA'])
     test_output = [(0, 2, 1), (2, 0, 1)]
-    assert algorithm(test_input, max_edits=1) == test_output
+    assert set_equal(algorithm(test_input, max_edits=1), test_output)
 
     test_input = pd.Series(['CAAA', 'CDDD', 'CADA'])
-    assert algorithm(test_input, max_edits=1) == test_output
+    assert set_equal(algorithm(test_input, max_edits=1), test_output)
 
 
 @pytest.mark.parametrize("algorithm", ALGORITHMS)
 def test_hamming(algorithm):
     test_input = ['CAAA', 'CDDD', 'CADA', 'CAAAD']
     test_output = [(0, 2, 1), (2, 0, 1)]
-    assert algorithm(test_input, custom_distance='hamming',
-                     max_edits=1) == test_output
+    assert set_equal(algorithm(test_input, custom_distance='hamming',
+                               max_edits=1), test_output)
 
 
 @pytest.mark.parametrize("algorithm", ALGORITHMS)
 def test_custom_distance(algorithm):
     test_input = ['CAAA', 'CDDD', 'CADA', 'CAAK']
     test_output = [(0, 2, 1), (0, 3, 1), (2, 0, 1), (3, 0, 1)]
-    assert algorithm(test_input, max_edits=1,
-                     custom_distance=distance) == test_output
+    assert set_equal(algorithm(test_input, max_edits=1,
+                               custom_distance=distance), test_output)
 
-    test_output = [(0, 2, 1), (2, 0, 1), (3, 0, 1)]
-    assert algorithm(test_input, max_edits=1, max_returns=1,
-                     custom_distance=distance) == test_output
+    output = set(algorithm(test_input, max_edits=1, max_returns=1,
+                           custom_distance=distance))
+    assert len(output) == 3 == len(output & set(test_output))
 
     test_output = []
-    assert algorithm(test_input, max_edits=1, max_returns=1,
-                     custom_distance=distance,
-                     max_custom_distance=0) == test_output
+    assert set_equal(algorithm(test_input, max_edits=1, max_returns=1,
+                               custom_distance=distance,
+                               max_custom_distance=0), test_output)
 
 
-@pytest.mark.skip(reason="very slow")
+# @pytest.mark.skip(reason="very slow")
 @pytest.mark.parametrize("algorithm", ALGORITHMS)
 def test_bulk(algorithm):
     combinations1 = product('ACD', repeat=2)
@@ -111,5 +117,4 @@ def test_bulk(algorithm):
         fallback_cache = fallback(test_input, max_edits=2)
 
     fuzzy_version = algorithm(test_input, max_edits=2, n_cpu=4)
-    def key_func(x): return str(x[0])+'_'+str(x[1])
-    assert sorted(fallback_cache) == sorted(fuzzy_version)
+    assert set_equal(fallback_cache, fuzzy_version)
