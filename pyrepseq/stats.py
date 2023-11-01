807c3d567f8cda8a1fca378d4341b0b09d27f335
jhenderson0 <hendersonjfd@gmail.com>
2023-11-01 14:03:46 +0000

    Add entropy into stats

--- pyrepseq/stats.py
+++ pyrepseq/stats.py
@@ -422,3 +422,84 @@ def overlap_coefficient(A, B):
     B = set(B)
     
     return len(A.intersection(B)) / min(len(A), len(B))
+<<<<<<< HEAD
+=======
+
+def shannon_entropy(df, features, by=None, base=2.0):
+    """Compute Shannon entropies
+    
+    Parameters
+    ----------
+    df : pandas DataFrame
+    features: list
+    by: string/list of strings
+    base: float
+
+    Returns
+    ----------: 
+    float
+    """
+    
+    if base is not None and base <= 0:
+        raise ValueError("`base` must be a positive number or `None`.")
+    
+    if type(features) != list:
+        features = [features]
+        
+    if by is None:
+        probabilities = df[features].value_counts(normalize=True)
+        entropy  = -(probabilities*np.log(probabilities)).sum()
+    
+    else:
+        if type(by) == list and len(by) > 1:
+            by = by[0]
+            
+        marginal_probabilities = df[by].value_counts(normalize=True)
+        
+        if type(by) != list:
+            joint_probabilities = df[features+[by]].value_counts(normalize=True)
+        else:
+            joint_probabilities = df[features+by].value_counts(normalize=True)
+
+        entropy = -(joint_probabilities*np.log(joint_probabilities/marginal_probabilities)).sum()
+        
+    if base is not None:
+        entropy /= np.log(base) 
+        
+    return entropy 
+
+def renyi2_entropy(df, features, by=None, base=2.0):
+    """Compute Renyi-Simpson entropies
+    
+    Parameters
+    ----------
+    df : pandas DataFrame
+    features: list
+    by: string/list of strings
+    base: float
+
+    Returns
+    ----------: 
+    float
+    """
+    
+    if base is not None and base <= 0:
+        raise ValueError("`base` must be a positive number or `None`.")
+    
+    if type(features) != list:
+        features = [features]
+            
+    if not by:
+        entropy = -np.log(pc_joint(df, features))
+    else:
+        entropy = -np.log(pc_conditional(df, by, features))
+    
+    if base is not None:
+        entropy /= np.log(base) 
+    
+<<<<<<< HEAD
+    return entropy
+=======
+    return entropy
+>>>>>>> 0fd7ecb (Add entropy into stats)
+>>>>>>> d026e48 (Add entropy into stats)
